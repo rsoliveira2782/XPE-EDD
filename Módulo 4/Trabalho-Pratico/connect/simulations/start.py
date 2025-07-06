431899a875eca6f1d02b3776315135310dec7d70
jpaulobf <jpaulobf@gmail.com>
2025-07-06 14:24:31 +0100

    adjustments

--- Módulo 4/Trabalho-Pratico/connect/simulations/start.py
+++ Módulo 4/Trabalho-Pratico/connect/simulations/start.py
@@ -2,7 +2,8 @@ import subprocess
 import sys
 
 # Instala as dependências
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--break-system-packages", "-r", "./simulations/requirements.txt"])
+
 
 # Executa o programa principal (substitua 'main.py' pelo seu script)
 # subprocess.run([sys.executable, "main.py"])
